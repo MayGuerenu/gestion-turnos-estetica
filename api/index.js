7daf94af28fce4e408a1f792058a37e66a272ea2
May <may@Mac-fe27ae2ac1de.fibertel.com.ar>
2026-02-09 23:58:40 -0300

    Eliminar dashboard mock y dejar dashboard real con API

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
-import app from "../src/app.js";
+import fs from "fs";
+import path from "path";
 
-export default app;
+export default function handler(req, res) {
+  const filePath = path.join(process.cwd(), "public", "index.html");
+  const html = fs.readFileSync(filePath, "utf8");
+
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send(html);
+}
