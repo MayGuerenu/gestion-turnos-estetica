6937e2aabde4128678b1886e43ea4fb8bb84a4d5
May <may@Mac-fe27ae2ac1de.fibertel.com.ar>
2026-02-10 20:08:25 -0300

    Arreglar rutas API en Vercel (catch-all)

--- api/[...all].js
+++ api/[...all].js
@@ -1,4 +1,7 @@
+
 import serverless from "serverless-http";
+
 import app from "../src/app.js";
 
 export default serverless(app);
+
--- api/health.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    project: "gestion-turnos-estetica",
-    hasSupabaseUrl: !!process.env.SUPABASE_URL,
-    hasServiceRoleKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
-    hasJwtSecret: !!process.env.JWT_SECRET
-  });
-}
